d1f38fead10bca981579462f763bf07f8d354455
Alexanderel04 <2004.estrada.lopez@gmail.com>
2023-07-18 14:01:19 -0600

    "Upload 18/07/2023"

--- app/components/aboutMe.js
+++ app/components/aboutMe.js
@@ -9,9 +9,9 @@ function AboutMe() {
 
       <div className='contenedor gray grid2'>
         <div className='contenedor'>
-            <p className={styles.moreBig}>Hello!, I'm <span>Alejandro</span></p>
+            <p className={styles.moreBig}>Hello!, I am <span>Alejandro</span></p>
             <div className='gap2'>
-              <p>I'm web developer with 4 years of experience</p>
+              <p>I am web developer with 4 years of experience</p>
               <p>I Know how to use different technologies in Front End and Back End, like: React, Java, JavaScript, PHP, Python, and more.</p>
             </div>
             
